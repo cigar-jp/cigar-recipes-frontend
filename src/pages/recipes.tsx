b42c490a3355aaec89fc55dff2ef8d03b5f6c9d6
cigar-jp <ryo.shiga.banana@gmail.com>
2023-04-25 10:36:57 +0900

    feat: page recipes

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -5,12 +5,15 @@ import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '@/components/commons/Layout'
 import { UserInfo } from '@/components/atoms/UserInfo'
 import { useQueryClient } from '@tanstack/react-query'
+import { RecipeForm } from '@/components/molecules/RecipeForm'
+import { RecipeList } from '@/components/organisms/RecipeList'
 
 const Recipes: NextPage = () => {
   const router = useRouter()
   const queryClient = useQueryClient()
 
   const signout = async () => {
+    queryClient.removeQueries(['recipes'])
     queryClient.removeQueries(['user'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signout`)
     router.push('/')
@@ -24,6 +27,10 @@ const Recipes: NextPage = () => {
       />
 
       <UserInfo />
+
+      <RecipeForm />
+
+      <RecipeList />
     </Layout>
   )
 }
